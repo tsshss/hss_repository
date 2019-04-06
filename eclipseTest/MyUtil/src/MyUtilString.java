f22a048191fa90031597f0438f976ecc6f71df5c
hisashi <hoge@email.com>
2019-04-06 14:23:42 +0900

    プロジェクトのコミット

---
+++ eclipseTest/MyUtil/src/MyUtilString.java
@@ -0,0 +1,21 @@
+public class MyUtilString {
+
+    public static void main(String[] args) {
+        
+        String[] strlist = "hjhjhjh.java kkjkkkkk.java kjlfjljlj.java".split(" ");
+
+        
+        strChange(strlist);
+        
+    }
+    
+    public static void strChange(String[] strlist) {
+        
+        for(String str : strlist) {
+            
+             System.out.println(str.replaceAll(".java", ".class") + ",");
+          
+        }
+    }
+
+}
